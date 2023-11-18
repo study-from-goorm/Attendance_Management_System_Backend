9dc9c67f0ae49881c1a8373eda12be6b630255db
k-minsik <smin58810@gmail.com>
2023-11-18 21:40:55 +0900

    feat: Init attendance management project

---
+++ src/main/java/goorm/attendancebook/AttendanceBookApplication.java
@@ -0,0 +1,13 @@
+package goorm.attendancebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceBookApplication.class, args);
+	}
+
+}
