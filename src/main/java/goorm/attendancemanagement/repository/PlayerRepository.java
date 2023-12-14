bafb189abc28c0e64c52087490a8e8dc1cfd7a13
Hyejin724 <imhaejin98@gmail.com>
2023-12-14 14:34:32 +0900

    feat: Add update/delete method to course/player

--- src/main/java/goorm/attendancemanagement/repository/PlayerRepository.java
+++ src/main/java/goorm/attendancemanagement/repository/PlayerRepository.java
@@ -15,4 +15,6 @@ public interface PlayerRepository extends JpaRepository<Player, Integer> {
     @Query("select p from Player p join fetch p.course where p.playerId = :id")
     List<Player> findByIdWithCourse(@Param("id") int id);
 
+    @Query("select p from Player p join fetch p.course where p.course.courseId = :courseId")
+    List<Player> findByCourse_CourseIdAndPlayerName(@Param("courseId") int courseId);
 }
